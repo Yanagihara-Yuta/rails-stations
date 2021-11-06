23ab501112e9af855a6bafa0cbae43cb4b2afcd5
Yanagihara-Yuta <yuta09029018438@gmail.com>
2021-11-06 19:06:14 +0900

    Rails_Insert

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,6 +1,19 @@
 class Admin::MoviesController < ApplicationController
   def index
     @title = "Management Page"
+      @movies = Movie.all
+  end
+
+  def new
+    @title = "Create Page"
+    @name = "タイトル"
+    @year = "公開年"
+    @description = "概要"
+    @image_url = "画像URL"
+    @is_showing = "上映中か"
+  end
+
+  def create
     @name = ""
     if request.post?
       @title = "PostManagement Page()"
@@ -14,25 +27,10 @@ class Admin::MoviesController < ApplicationController
       @sql_insert = Movie.new(name:name,year:year,description:description,image_url:image_url,is_showing:is_showing,created_at:created_at,updated_at:updated_at)
       # sql_insert.execute(name,year,description,image_url,is_showing,created_at,updated_at)
       if @sql_insert.save!
-
       else
         flash.now[:alert] = 'メッセージを入力してください。'
         render action: 'new'
       end
     end
-      @movies = Movie.all
-  end
-
-  def new
-    @title = "Create Page"
-    @name = "タイトル"
-    @year = "公開年"
-    @description = "概要"
-    @image_url = "画像URL"
-    @is_showing = "上映中か"
-  end
-
-  def insert
-    render action: "create"
   end
 end
