1105673983799268fe64da2f33d15110c34c01e9
Yanagihara-Yuta <yuta09029018438@gmail.com>
2021-11-06 23:19:52 +0900

    Rails_Insert

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,6 @@ class Movie < ApplicationRecord
     validates :name, uniqueness: true
     validates :name,    length: { maximum: 160 }
     validates :year,    length: { maximum: 45 }
-    validates :is_showing,    length: { maximum: 1 }
+    validates :is_showing,    numericality: {greater_than: -1,less_than: 2}
     validates :image_url,    length: { maximum: 150 }
 end
