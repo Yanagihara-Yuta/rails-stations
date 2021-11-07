1a4df051f129ef9f4f33479cadbb51a875adfc1b
Yanagihara-Yuta <yuta09029018438@gmail.com>
2021-11-07 11:44:43 +0900

    Rails_Insert

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -2,16 +2,6 @@ class Admin::MoviesController < ApplicationController
   def index
     @title = "Management Page"
     @name = ""
-    @movies = Movie.all
-  end
-
-  def new
-    @title = "Create Page"
-    @name = "タイトル"
-    @year = "公開年"
-    @description = "概要"
-    @image_url = "画像URL"
-    @is_showing = "上映中か"
     if request.post?
       @title = "PostManagement Page()"
       name = params[:name]
@@ -24,13 +14,22 @@ class Admin::MoviesController < ApplicationController
       @sql_insert = Movie.new(name:name,year:year,description:description,image_url:image_url,is_showing:is_showing,created_at:created_at,updated_at:updated_at)
       # sql_insert.execute(name,year,description,image_url,is_showing,created_at,updated_at)
       if @sql_insert.save
-        render action: 'index'
       else
         flash.now[:alert] = 'メッセージを入力してください。'
         render action: 'new'
       end
     end
+    @movies = Movie.all
+  end
 
+  def new
+    @title = "Create Page"
+    @name = "タイトル"
+    @year = "公開年"
+    @description = "概要"
+    @image_url = "画像URL"
+    @is_showing = "上映中か"
+    
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get '/movies', to: 'movies#index'
     get '/movies/new', to: 'movies#new'
-    post '/movies/new', to: 'movies#new'
+    post '/movies', to: 'movies#index'
   end
     get "/", to: "movies#index"
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
