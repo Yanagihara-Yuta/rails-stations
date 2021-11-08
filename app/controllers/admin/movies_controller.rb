1b25d3b20c2a7b790b96d1ff794f6ddb1ce68b02
Yanagihara-Yuta <yuta09029018438@gmail.com>
2021-11-08 12:53:28 +0900

    Rails_Insert

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -16,9 +16,9 @@ class Admin::MoviesController < ApplicationController
         is_showing = params[:is_showing]
         created_at = Time.zone.now()
         updated_at = Time.zone.now()
-        @sql_insert = Movie.create(name:name,year:year,description:description,image_url:image_url,is_showing:is_showing,created_at:created_at,updated_at:updated_at)
+        @sql_insert = Movie.new(name:name,year:year,description:description,image_url:image_url,is_showing:is_showing,created_at:created_at,updated_at:updated_at)
         # sql_insert.execute(name,year,description,image_url,is_showing,created_at,updated_at)
-            if @sql_insert.valid?
+            if @sql_insert.save
                 flash.now[:alert] = 'メッセージを入力した。'
                 redirect_to admin_movies_path
             else
