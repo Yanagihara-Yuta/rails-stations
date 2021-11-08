4f228bcdda2565094c5c0867b5effd5fb565bc9f
Yanagihara-Yuta <yuta09029018438@gmail.com>
2021-11-08 19:32:05 +0900

    Rails_Insert

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -10,11 +10,11 @@ class Admin::MoviesController < ApplicationController
     end
     def create 
         params_create = params.require(:movie).permit(:name, :year, :is_showing, :description, :image_url)
-        @movie = Movie.create(params_create)
+        @movie = Movie.new(params_create)
         # sql_insert.execute(name,year,description,image_url,is_showing,created_at,updated_at)
-            if @movie.valid?
+            if @movie.save
                 flash.now[:alert] = 'メッセージを入力した。'
-                redirect_to "/movies_index"
+                redirect_to "/admin/movies"
             else
                 flash[:alert] = 'メッセージを入力してください。'
                 redirect_to admin_movies_new_path , status:200
