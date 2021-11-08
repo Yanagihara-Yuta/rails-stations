96283bf995908d04eaffb77a5fe2f9f5a577eb74
Yanagihara-Yuta <yuta09029018438@gmail.com>
2021-11-08 11:23:25 +0900

    Rails_Insert

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -20,11 +20,10 @@ class Admin::MoviesController < ApplicationController
         # sql_insert.execute(name,year,description,image_url,is_showing,created_at,updated_at)
             if @sql_insert.save
                 flash.now[:alert] = 'メッセージを入力した。'
-                render action: 'index'
+                redirect_to admin_movies_new_path
             else
                 flash.now[:alert] = 'メッセージを入力してください。'
                 redirect_to admin_movies_new_path
-
             end
     end
 
