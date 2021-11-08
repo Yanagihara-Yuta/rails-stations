d7d883ee5df9a0b33cd6bf6e8ccb811d51622a02
Yanagihara-Yuta <yuta09029018438@gmail.com>
2021-11-08 12:46:22 +0900

    Rails_Insert

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -23,7 +23,7 @@ class Admin::MoviesController < ApplicationController
                 redirect_to admin_movies_path
             else
                 flash[:alert] = 'メッセージを入力してください。'
-                render :new
+                redirect_to :new
             end
     end
 end
