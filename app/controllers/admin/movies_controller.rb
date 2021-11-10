328b40b0c930675a5589951f7ccbd279bc9e17af
Yanagihara-Yuta <yuta09029018438@gmail.com>
2021-11-10 13:38:06 +0900

    Rails_Update

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,10 +1,11 @@
 class Admin::MoviesController < ApplicationController
+# R機能
     def index
         @title = "Management Page"
         @name = ""
         @movies = Movie.all
     end
-
+# C機能
     def new
         @movie = Movie.new
     end
@@ -19,6 +20,22 @@ class Admin::MoviesController < ApplicationController
                 flash[:alert] = 'メッセージを入力してください。'
                 redirect_to admin_movies_new_path , status:200
             end
-
+    end
+# U機能
+    def edit
+        @movie = Movie.find(params[:id])
+    end
+    
+    def update
+        @movie = Movie.find(params[:id])
+        params_update = params.require(:movie).permit(:name, :year, :is_showing, :description, :image_url, :updated_at)
+        if @movie.update(params_update)
+            flash.now[:alert] = 'メッセージを入力した。'
+            redirect_to "/admin/movies"
+        else
+            flash[:alert] = 'メッセージを入力してください。'
+            redirect_to action: :edit,id:@movie.id
+        end
+            
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     get '/movies' => 'movies#index'
     get '/movies/new' => 'movies#new'
     post '/movies' => 'movies#create'
+    get 'movies/:id' => 'movies#edit'
+    patch 'movies/:id' => 'movies#update'
   end
     get "/" => "movies#index"
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
