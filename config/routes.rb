4c62b75a2c56876223039485daf9f8e7058dca81
Yanagihara-Yuta <yuta09029018438@gmail.com>
2021-11-11 17:48:06 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get '/movies' => 'movies#index'
-    get '/movies/new' => 'movies#new'
-    post '/movies' => 'movies#create'
-    get 'movies/:id' => 'movies#edit'
-    patch 'movies/:id' => 'movies#update'
+    resources :movies
   end
-    get "/" => "movies#index"
+  resources :movies, only: [ :index ]
+
+  get '*path', to: 'application#rescue404'
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
