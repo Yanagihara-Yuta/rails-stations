3ce02351a45fefad07882f429cb2c4cd6c1f1808
Yanagihara-Yuta <yuta09029018438@gmail.com>
2021-11-11 19:41:07 +0900

    Rails_Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :movies
+    resource :movies
   end
     get "/" => "movies#index"
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
