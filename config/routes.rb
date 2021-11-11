e05323439e991b1badc22d8932028948af3f1137
Yanagihara-Yuta <yuta09029018438@gmail.com>
2021-11-11 19:37:40 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get '/movies' => 'movies#index'
-    get '/movies/new' => 'movies#new'
-    post '/movies' => 'movies#create'
-    get 'movies/:id' => 'movies#edit'
-    patch 'movies/:id' => 'movies#update'
-    put 'movies/:id' => 'movies#update'
+    resources :movies
   end
     get "/" => "movies#index"
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
