95279158e6e0eca536028585f012243502f84a80
Yanagihara-Yuta <yuta09029018438@gmail.com>
2021-11-19 20:09:23 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     get "/movies" => "movies#index"
     get "/movies/search" => "movies#search"
     get "/movies/sheets" => "movies#sheets"
-    get "/sheets" => "sheets#index"
+    get "/sheets" => "sheets#sheets"
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
